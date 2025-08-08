67399c5bf281015f73264a38d8ed642935f5dad5
Jakub Piotr Cłapa <jakub@hume.ai>
2025-08-08 14:39:16 +0000

    Added WSSink & co

---
+++ experiments/wsds.py
@@ -0,0 +1,134 @@
+import marimo
+
+__generated_with = "0.14.11"
+app = marimo.App(width="medium")
+
+with app.setup:
+    import random, os
+    from pathlib import Path
+    from contextlib import contextmanager
+    from dataclasses import dataclass
+
+    import pyarrow
+
+
+@app.function
+def indented(prefix, obj):
+    prefix = prefix + ': '
+    lines = str(obj).split('\n')
+    return "\n".join(p + ln for p,ln in zip([prefix] + [" "*len(prefix)] * (len(lines)-1), lines))
+
+
+@app.class_definition
+@dataclass(frozen=True)
+class SampleFormatChanged(BaseException):
+    old_schema: pyarrow.Schema
+    new_schema: pyarrow.Schema
+
+    def __str__(self):
+        return (f'The dataset format changed:\n\n'
+                f'{indented("  OLD", self.old_schema)}\n\n'
+                f'{indented("  NEW", self.new_schema)}')
+
+
+@app.cell
+def _():
+    err = SampleFormatChanged(
+        pyarrow.schema([('a', pyarrow.int64()), ('b', pyarrow.string())]),
+        pyarrow.schema([('b', pyarrow.string()), ('a', pyarrow.int64())]),
+    )
+    print(err)
+    return
+
+
+@app.class_definition
+class WSBatchedSink:
+    def __init__(self,
+                 fname, # final output file name, intermediate output goes into a temporary file
+                 batch_size=16,
+                 min_batch_size_bytes=False,
+                 compression='zstd',
+                 throwaway=False, # discard the temp file, useful for testing and benchmarking
+                ):
+        self.fname = fname
+        self.batch_size = batch_size
+        self.min_batch_size_bytes = min_batch_size_bytes
+        self.compression = compression
+        self.throwaway = throwaway
+
+        self._buffer = []
+        self._sink = None
+
+    def write(self, x):
+        self._buffer.append(x)
+        if len(self._buffer) >= self.batch_size:
+            self.write_batch(self._buffer)
+
+    # TODO: test writing batches of data straight from a PyTorch batched processing loop
+    def write_batch(self, b, flush=False):
+        record = pyarrow.RecordBatch.from_pylist(b)
+        if self._sink is None:
+            if not flush and self.min_batch_size_bytes:
+                if record.nbytes < self.min_batch_size_bytes and self.batch_size < 16384:
+                    self.batch_size *= 2
+                    return
+            schema = record.schema.with_metadata({'batch_size': str(len(b))})
+            self._sink = pyarrow.RecordBatchFileWriter(
+                self.fname, schema,
+                options=pyarrow.ipc.IpcWriteOptions(compression=self.compression))
+            self._sink_schema = schema
+        if record.schema != self._sink_schema:
+            raise SampleFormatChanged(self._sink_schema, record.schema)
+        self._sink.write(record)
+        self._buffer.clear()
+
+    def close(self):
+        if self._buffer: self.write_batch(self._buffer, flush=True) # flush the last batch
+        assert self._sink is not None, "closing a WSSink that was never written to"
+        self._sink.close()
+
+    def __enter__(self):
+        assert self._sink is None, "WSSink is not re-entrant"
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type is None:
+            self.close()
+
+
+@app.class_definition
+class AtomicFile:
+    def __init__(self, fname, ephemeral=False):
+        self.fname = fname
+        self.ephemeral = ephemeral
+
+    def __enter__(self):
+        Path(self.fname).parent.mkdir(exist_ok=True, parents=True)
+        self._tmp_name = self.fname + ('.%010x' % random.getrandbits(40))
+        return self._tmp_name
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type is None and not self.ephemeral:
+            os.rename(self._tmp_name, self.fname)
+        else:
+            # print(f"Deleting partial file: {self._tmp_name}")
+            if Path(self._tmp_name).exists():
+                os.remove(self._tmp_name)
+
+
+@app.function
+@contextmanager
+def WSSink(
+    fname, # final output file name, intermediate output goes into a temporary file
+    batch_size=16,
+    compression='zstd',
+    min_batch_size_bytes=False,
+    ephemeral=False, # discard the temp file, useful for testing and benchmarking
+):
+    with AtomicFile(fname, ephemeral) as fname:
+        with WSBatchedSink(fname, batch_size, min_batch_size_bytes, compression) as sink:
+            yield sink
+
+
+if __name__ == "__main__":
+    app.run()
