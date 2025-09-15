9dca3e4e2b77981038ca8270d9f4cd4b74e9b584
Jakub Piotr Cłapa <jakub@hume.ai>
2025-09-15 09:43:39 +0000

    Export AtomicFile from the main module

--- hume_wsds/__init__.py
+++ hume_wsds/__init__.py
@@ -1,4 +1,4 @@
 from .ws_dataset import WSDataset
 from .ws_shard import WSSourceAudioShard
 from .ws_sample import WSSample
-from .ws_sink import WSSink, SampleFormatChanged
+from .ws_sink import WSSink, SampleFormatChanged, AtomicFile
