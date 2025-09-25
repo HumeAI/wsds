4473bf41bde0f7f37bcfc865c4db86be486c375f
Jakub Piotr Cłapa <jakub@hume.ai>
2025-09-25 10:44:17 +0000

    WSSample: implement more of the dict API
    This also let's people add keys to the sample objects.

--- hume_wsds/ws_sample.py
+++ hume_wsds/ws_sample.py
@@ -1,10 +1,11 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 @dataclass(frozen=True, slots=True)
 class WSSample:
     dataset: "WSDataset"
     shard_name: str
     offset: int
+    overrides: dict = field(default_factory=dict)
 
     def get_key(self):
         return self.dataset.get_key(self.shard_name, self.offset)
@@ -24,10 +25,18 @@ class WSSample:
         yield from (v for k, v in self.items())
 
     def __getitem__(self, field):
+        if field in self.overrides:
+            return self.overrides[field]
         return self.dataset.get_sample(self.shard_name, field, self.offset)
 
+    def __setitem__(self, field, value):
+        self.overrides[field] = value
+
+    def get(self, field:str, default=None):
+        return self[field] if field in self else default
+
     def __contains__(self, field):
-        return field in self.dataset.fields.keys()
+        return field in self.overrides or field in self.dataset.fields.keys()
 
     def __repr__(self):
         r = f"WSSample({repr(self.dataset)}, shard_name={repr(self.shard_name)}, offset={repr(self.offset)}, fields={'{'}\n"
