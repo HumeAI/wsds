8dfedb28fcedc39ee845b989beafb00fbde5e708
aliceebaird <alice.e.baird@gmail.com>
2025-10-03 18:40:52 +0000

    clean up ws_sample

--- hume_wsds/ws_sample.py
+++ hume_wsds/ws_sample.py
@@ -13,16 +13,14 @@ class WSSample:
 
     def get_audio(self, audio_columns=None):
         candidates = audio_columns or self.dataset._audio_file_keys
-    
+
         # if we normalized into a single 'audio' field
         if "audio" in self:
             return self["audio"]
-    
+
         r = self.get_one_of(*candidates)
         if not r:
-            raise KeyError(
-                f"No audio column (tried {candidates}) found among: {list(self.keys())}"
-            )
+            raise KeyError(f"No audio column (tried {candidates}) found among: {list(self.keys())}")
         return r
 
     def keys(self):
