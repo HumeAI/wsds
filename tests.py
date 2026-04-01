132d80b911939d7e6acb4efae9a7d41fc2456487
Jakub Piotr Cłapa <jakub@hume.ai>
2026-04-01 12:14:02 +0000

    Audio decoding rework (NFY)

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import doctest
 import unittest
 
 import wsds
-from wsds import ws_dataset, ws_shard, ws_sink
+from wsds import ws_dataset, ws_shard, ws_sink, ws_audio, audio_codec
 
 
 def load_tests(loader, tests, ignore):
@@ -10,6 +10,8 @@ def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocTestSuite(ws_dataset))
     tests.addTests(doctest.DocTestSuite(ws_shard))
     tests.addTests(doctest.DocTestSuite(ws_sink))
+    tests.addTests(doctest.DocTestSuite(ws_audio))
+    tests.addTests(doctest.DocTestSuite(audio_codec))
     tests.addTests(doctest.DocFileSuite("README.md"))
     return tests
 
